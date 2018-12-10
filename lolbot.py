a3304f90891d23c54760c15696d4e99d3cc63b55
Kenni Bawden <kenni.bawden@gmail.com>
2018-12-10 17:26:40 +1100

    Added text output

--- lolbot.py
+++ lolbot.py
@@ -1,13 +1,18 @@
 # Import flask with the requests object
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 # Create the web server
 app = Flask(__name__)
 
-# This bot lols at every thing it gets sent!
-def lol_bot():
-    text = request.form.get('text')
-    return f'lol {text}'
-
 # You can message lol_bot via <your website>/lol
-app.route('/lol', methods=['GET', 'POST'])(lol_bot)
+@app.route('/lol', methods=['GET', 'POST'])
+def lol_bot():
+	# This bot lols at every thing it gets sent!
+    text = request.form.get('text')
+    return jsonify({
+		'response_type': 'in_channel',
+		'text': f'lol {text}',
+	})
+
+if __name__ == '__main__':
+	app.run()
